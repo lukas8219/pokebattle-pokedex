f60e674417daa64e769064b05abd9eb4df42069c
Lucas <lucas.c4d@gmail.com>
2021-07-13 08:21:38 -0300

    added CORS configuration

--- src/main/java/com/pokebattle/pokedex/config/PokeDexConfig.java
+++ src/main/java/com/pokebattle/pokedex/config/PokeDexConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class PokeDexConfig {
+public class PokeDexConfig implements WebMvcConfigurer {
 
     @Bean
     public MessageSource messageSource() {
@@ -26,4 +28,9 @@ public class PokeDexConfig {
         return bean;
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**");
+    }
 }
