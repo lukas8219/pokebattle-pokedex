971fb70bbc59670f2133d59db76653f75c0cb008
Lucas <lucas.c4d@gmail.com>
2021-09-23 19:05:03 -0300

    added aspect

---
+++ src/main/java/com/pokebattle/pokedex/annotations/LogIt.java
@@ -0,0 +1,2 @@
+package com.pokebattle.pokedex.annotations;public @interface LogIt {
+}
--- src/main/java/com/pokebattle/pokedex/service/PokeDexService.java
+++ src/main/java/com/pokebattle/pokedex/service/PokeDexService.java
@@ -7,7 +7,6 @@ import com.pokebattle.pokedex.decorator.PaginationDecorator;
 import com.pokebattle.pokedex.mapper.PokemonMapper;
 import com.pokebattle.pokedex.repository.PokemonRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -15,7 +14,6 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class PokeDexService {
 
     private final PokemonRepository pokemonRepository;
@@ -23,10 +21,8 @@ public class PokeDexService {
     private final PokemonMapper pokemonMapper;
 
     public PokemonDTO getPokemon(Long id) {
-        log.info("Received request for Pokemon with id : {}", id);
         var pokemon = pokemonRepository.findById(id);
         var dto = pokemonMapper.toDTO(pokemon);
-        log.info("Returning : {}", dto);
         return dto;
     }
 
@@ -34,7 +30,6 @@ public class PokeDexService {
         List<String> list = name != null ? List.of(name) : Collections.emptyList();
         var pagination = paginationDecorator.from(pageNumber, pageSize, sortBy, list);
         var pokemonList = pokemonRepository.searchPokemonList(pagination);
-        log.info("Returning Pokemon List : {}", pokemonList);
         return paginationDecorator.toPaginationResult(pagination, pokemonList);
     }
 }
